bf86f52f6409145f9492e21c831a6a1bc1e95abe
River Pearl <riverpearl0115@gmail.com>
2016-07-18 17:13:40 +0900

    Simple intent example

--- app/src/main/java/com/tacademy/sampleapplicationcomponent/MainActivity.java
+++ app/src/main/java/com/tacademy/sampleapplicationcomponent/MainActivity.java
@@ -1,13 +1,52 @@
 package com.tacademy.sampleapplicationcomponent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputView;
+    TextView resultView;
+
+    private final static int RC_OTHER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputView = (EditText)findViewById(R.id.edit_input);
+        resultView = (TextView)findViewById(R.id.text_result);
+        Button btn = (Button)findViewById(R.id.btn_other);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                String input = inputView.getText().toString();
+                intent.putExtra(OtherActivity.EXTRA_KEYWORD, input);
+                intent.putExtra(OtherActivity.EXTRA_AGE, 42);
+                startActivityForResult(intent, RC_OTHER); // 여기서의 intent = onActivityResult의 requestCode
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_OTHER) {
+            if (resultCode == Activity.RESULT_OK) {
+                String text = data.getStringExtra(OtherActivity.RESULT_MESSAGE);
+                resultView.setText(text);
+            }
+            else Toast.makeText(this, "Result Canceled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/tacademy/sampleapplicationcomponent/OtherActivity.java
+++ app/src/main/java/com/tacademy/sampleapplicationcomponent/OtherActivity.java
@@ -1,14 +1,49 @@
 package com.tacademy.sampleapplicationcomponent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    TextView messageView;
+    EditText inputResultView;
+
+    public final static String EXTRA_KEYWORD = "keyword";
+    public final static String EXTRA_AGE = "age";
+    public final static String RESULT_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        messageView = (TextView)findViewById(R.id.text_message);
+        inputResultView = (EditText)findViewById(R.id.edit_input_result);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(EXTRA_KEYWORD);
+        int age = intent.getIntExtra(EXTRA_AGE, 0);
+        // 뒤에 default 값을 정의할 수 있다. (값이 넘어오지 않을 경우 0으로 초기화)
+        // String은 default 값을 정의하지 않아도 자동으로 null로 처리됨.
+
+        messageView.setText(text + ", " + age);
+
+        Button btn = (Button)findViewById(R.id.btn_finish);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = inputResultView.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra(RESULT_MESSAGE, result);
+                setResult(Activity.RESULT_OK, intent); // 매개변수 차례대로 onActivityResult의 resultCode, data
+                finish();
+            }
+        });
     }
 }
