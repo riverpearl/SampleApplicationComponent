d8635b027b48bdcd28acd200aa0c76dce83b0965
River Pearl <riverpearl0115@gmail.com>
2016-07-21 18:42:43 +0900

    sms, screen on/off receiver

--- app/src/main/java/com/tacademy/sampleapplicationcomponent/MyService.java
+++ app/src/main/java/com/tacademy/sampleapplicationcomponent/MyService.java
@@ -1,7 +1,10 @@
 package com.tacademy.sampleapplicationcomponent;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -41,7 +44,22 @@ public class MyService extends Service {
                 }
             }
         }).start();
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+
+        registerReceiver(mReceiver, filter);
     }
+
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
+                Toast.makeText(context, "Screen On", Toast.LENGTH_SHORT).show();
+            else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF))
+                Log.i(TAG, "Screen Off");
+        }
+    };
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -55,5 +73,6 @@ public class MyService extends Service {
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
 
         isRunning = false;
+        unregisterReceiver(mReceiver);
     }
 }
