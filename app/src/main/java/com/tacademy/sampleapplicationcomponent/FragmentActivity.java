75c1fc6822055a5268fa54149f94fa454640ad05
River Pearl <riverpearl0115@gmail.com>
2016-07-21 15:23:55 +0900

    argument example

--- app/src/main/java/com/tacademy/sampleapplicationcomponent/FragmentActivity.java
+++ app/src/main/java/com/tacademy/sampleapplicationcomponent/FragmentActivity.java
@@ -1,12 +1,19 @@
 package com.tacademy.sampleapplicationcomponent;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class FragmentActivity extends AppCompatActivity {
+public class FragmentActivity extends AppCompatActivity implements OneFragment.OnMessageCallBack {
+    // 콜백 인터페이스를 상속받고 메소드를 재정의하면 OtherFragmentActivity는 같은 fragment 참조해도 다르게 동작함
+
+    private static final String TAG_ONE = "one";
+    private static final String TAG_TWO = "two";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +24,15 @@ public class FragmentActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                OneFragment one = new OneFragment();
-                ft.replace(R.id.container, one);
-                ft.commit();
+                Fragment old = getSupportFragmentManager().findFragmentByTag(TAG_ONE);
+                if (old == null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    //OneFragment one = new OneFragment();
+                    OneFragment one = OneFragment.newInstance("hahaha");
+                    ft.replace(R.id.container, one, TAG_ONE);
+                    ft.commit();
+                }
+
             }
         });
 
@@ -28,11 +40,28 @@ public class FragmentActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                TwoFragment two = new TwoFragment();
-                ft.replace(R.id.container, two);
-                ft.commit();
+                Fragment old = getSupportFragmentManager().findFragmentByTag(TAG_TWO);
+                if (old == null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    TwoFragment two = new TwoFragment();
+                    ft.replace(R.id.container, two, TAG_TWO);
+                    ft.commit();
+                }
             }
         });
+
+        btn = (Button)findViewById(R.id.btn_other);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FragmentActivity.this, OtherFragmentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void displayMessage(String message) {
+        Toast.makeText(this, "message : " + message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/tacademy/sampleapplicationcomponent/OneFragment.java
+++ app/src/main/java/com/tacademy/sampleapplicationcomponent/OneFragment.java
@@ -1,7 +1,9 @@
 package com.tacademy.sampleapplicationcomponent;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +18,43 @@ import android.widget.TextView;
  */
 public class OneFragment extends Fragment {
 
+    public interface OnMessageCallBack {
+        public void displayMessage(String message);
+    }
+
+    OnMessageCallBack callBack;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnMessageCallBack)
+            callBack = (OnMessageCallBack)context;
+    }
+
+    private static final String KEY_INPUT = "input";
+
+    public static OneFragment newInstance(String input) {
+        OneFragment fragment = new OneFragment();
+        Bundle b = new Bundle();
+        b.putString(KEY_INPUT, input);
+        fragment.setArguments(b);
+        return fragment;
+    }
 
     public OneFragment() {
         // Required empty public constructor
+    }
+
+    String inputText;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle b = getArguments();
+
+        if (b != null)
+            inputText = b.getString(KEY_INPUT);
     }
 
     EditText inputView;
@@ -31,13 +67,19 @@ public class OneFragment extends Fragment {
         //return inflater.inflate(R.layout.fragment_one, container, false);
         // 윗줄보다 아랫줄처럼 해서 view를 return 시켜주는 게 더 좋음
         View view = inflater.inflate(R.layout.fragment_one, container, false); // 이렇게 해서 view를 return 시켜주는 게 더 좋음
+
         inputView = (EditText)view.findViewById(R.id.edit_input);
+        inputView.setText(inputText);
         messageView = (TextView)view.findViewById(R.id.text_message);
+
         Button btn = (Button)view.findViewById(R.id.btn_send);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 messageView.setText(inputView.getText().toString());
+
+                if (callBack != null)
+                    callBack.displayMessage(inputView.getText().toString());
             }
         });
 
